358e28f8fa34762c9db51056cd48021523eb278c
Wilson Zheng <78946799+wjz224@users.noreply.github.com>
2025-06-13 23:03:34 -0400

    fixed shadcn bug

--- frontend/budgetai/src/components/BudgetPage.jsx
+++ frontend/budgetai/src/components/BudgetPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from './Web/Sidebar';
 import '../css/Dashboard.css';
+import { Button } from "@/components/ui/button"
 
 function Budgets() {
 
@@ -10,9 +11,7 @@ function Budgets() {
             <main className="dashboard-main-content">
                 <div className="page-header">
                     <h2>Budgets</h2>
-                    <button>
-                        New Budget
-                    </button>
+                    <Button>New Budget</Button>
                 </div>
                 <div className="page-header"></div>
                 <p>hello</p>
