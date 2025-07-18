0a2b2776a57836e70584390275ff7607b27fa543
Wilson Zheng <78946799+wjz224@users.noreply.github.com>
2025-07-18 00:03:26 -0400

    fixed pem

--- frontend/budgetai/vite.config.js
+++ frontend/budgetai/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
     plugins: [react()],
     resolve: {
@@ -10,12 +12,14 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
-    server: {
-        host: '127.0.0.1',
-        https: {
-            key: fs.readFileSync(path.resolve(__dirname, '../../key.pem')),
-            cert: fs.readFileSync(path.resolve(__dirname, '../../cert.pem')),
-        },
-        port: 3000, // or your preferred port
-  },
+    server: isDev
+        ? {
+            host: '127.0.0.1',
+            https: {
+                key: fs.readFileSync(path.resolve(__dirname, '../../key.pem')),
+                cert: fs.readFileSync(path.resolve(__dirname, '../../cert.pem')),
+            },
+            port: 3000,
+        }
+        : undefined,
 });
